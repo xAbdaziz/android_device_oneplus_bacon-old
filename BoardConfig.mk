a0ab7e19d97f6d9ade4b6613207f7890880dd1dd
xawlw <abdulazizawlw@gmail.com>
2019-05-14 14:31:03 +0300

    kernel: compile using HyperTC

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,8 @@ BOARD_DTBTOOL_ARGS := -2
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := fz_bacon_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
+KERNEL_TOOLCHAIN := /home/arch/msm/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-linux-eabi-HyperTC-4.9/bin
 PRODUCT_VENDOR_KERNEL_HEADERS :=  $(DEVICE_PATH)/kernel-headers
 
 # ANT+
